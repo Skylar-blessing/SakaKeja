bea2a54b0e0a96597f56f1bc0e7c261a585b6ddb
stephen <skaranja654@gmail.com>
2023-07-30 18:26:06 +0300

    new tables

--- backend/app.py
+++ backend/app.py
@@ -4,25 +4,351 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from config import Config
-from sqlalchemy.exc import IntegrityError
 
-db = SQLAlchemy()
-migrate = Migrate()
-api = Api()
+app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    CORS(app)
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    api.init_app(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app)
 
 
-    return app
+from models import User, Payment, Property, MoveAssistance, Review
+
+class IndexResource(Resource):
+    def get(self):
+        return {'message': 'Karibu sakakeja'}
+
+api.add_resource(IndexResource, '/')
+
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(jsonify(users), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_user = User(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            phone_number=data['phone_number'],
+            password=data['password'],
+            user_type=data['user_type']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        response_dict = new_user.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+
+api.add_resource(Users, "/users")
+
+class User_by_Id(Resource):
+    def get(self, id):
+        user = User.query.get(id)
+        if not user:
+            return make_response(jsonify({"error": "User not found"}), 404)
+
+        response_dict = user.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def patch(self, id):
+        user = User.query.get(id)
+        if not user:
+            return make_response(jsonify({"error": "User not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(user, attr, value)
+
+        db.session.commit()
+        response_dict = user.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def delete(self, id):
+        user = User.query.get(id)
+        if not user:
+            return make_response(jsonify({"error": "User not found"}), 404)
+
+        db.session.delete(user)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted successfully"}
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(User_by_Id, "/users/<int:id>")
+
+class Properties(Resource):
+    def get(self):
+        properties = [property.to_dict() for property in Property.query.all()]
+        return make_response(jsonify(properties), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_property = Property(
+            owner_id=data['owner_id'],
+            number_of_rooms=data['number_of_rooms'],
+            categories=data['categories'],
+            location=data['location'],
+            price=data['price'],
+            description=data['description'],
+            rating=data['rating'],
+            image_urls=data['image_urls']
+        )
+        db.session.add(new_property)
+        db.session.commit()
+
+        response_dict = new_property.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+
+api.add_resource(Properties, "/properties")
+
+class Property_by_Id(Resource):
+    def get(self, id):
+        property = Property.query.get(id)
+        if not property:
+            return make_response(jsonify({"error": "Property not found"}), 404)
+
+        response_dict = property.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def patch(self, id):
+        property = Property.query.get(id)
+        if not property:
+            return make_response(jsonify({"error": "Property not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(property, attr, value)
+
+        db.session.commit()
+        response_dict = property.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def delete(self, id):
+        property = Property.query.get(id)
+        if not property:
+            return make_response(jsonify({"error": "Property not found"}), 404)
+
+        db.session.delete(property)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted successfully"}
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(Property_by_Id, "/properties/<int:id>")
+
+class Payments(Resource):
+    def get(self):
+        payments = [payment.to_dict() for payment in Payment.query.all()]
+        return make_response(jsonify(payments), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_payment = Payment(
+            amount=data['amount'],
+            payment_date=data['payment_date'],
+            status=data['status'],
+            tenant_id=data['tenant_id'],
+            property_id=data['property_id']
+        )
+        db.session.add(new_payment)
+        db.session.commit()
+
+        response_dict = new_payment.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+
+api.add_resource(Payments, "/payments")
+
+class Payment_by_Id(Resource):
+    def get(self, id):
+        payment = Payment.query.get(id)
+        if not payment:
+            return make_response(jsonify({"error": "Payment not found"}), 404)
+
+        response_dict = payment.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def patch(self, id):
+        payment = Payment.query.get(id)
+        if not payment:
+            return make_response(jsonify({"error": "Payment not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(payment, attr, value)
+
+        db.session.commit()
+        response_dict = payment.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def delete(self, id):
+        payment = Payment.query.get(id)
+        if not payment:
+            return make_response(jsonify({"error": "Payment not found"}), 404)
+
+        db.session.delete(payment)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted successfully"}
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(Payment_by_Id, "/payments/<int:id>")
+
+class MoveAssistances(Resource):
+    def get(self):
+        move_assistances = [move.to_dict() for move in MoveAssistance.query.all()]
+        return make_response(jsonify(move_assistances), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_move = MoveAssistance(
+            service_details=data['service_details'],
+            status=data['status'],
+            tenant_id=data['tenant_id']
+        )
+        db.session.add(new_move)
+        db.session.commit()
+
+        response_dict = new_move.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+
+api.add_resource(MoveAssistances, "/move_assistances")
+
+class MoveAssistance_by_Id(Resource):
+    def get(self, id):
+        move = MoveAssistance.query.get(id)
+        if not move:
+            return make_response(jsonify({"error": "MoveAssistance not found"}), 404)
+
+        response_dict = move.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def patch(self, id):
+        move = MoveAssistance.query.get(id)
+        if not move:
+            return make_response(jsonify({"error": "MoveAssistance not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(move, attr, value)
+
+        db.session.commit()
+        response_dict = move.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def delete(self, id):
+        move = MoveAssistance.query.get(id)
+        if not move:
+            return make_response(jsonify({"error": "MoveAssistance not found"}), 404)
+
+        db.session.delete(move)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted successfully"}
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(MoveAssistance_by_Id, "/move_assistances/<int:id>")
+
+class Reviews(Resource):
+    def get(self):
+        reviews = [review.to_dict() for review in Review.query.all()]
+        return make_response(jsonify(reviews), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_review = Review(
+            review_text=data['review_text'],
+            rating=data['rating'],
+            tenant_id=data['tenant_id'],
+            property_id=data['property_id']
+        )
+        db.session.add(new_review)
+        db.session.commit()
+
+        response_dict = new_review.to_dict()
+        response = make_response(jsonify(response_dict), 201)
+
+        return response
+
+api.add_resource(Reviews, "/reviews")
+
+class Review_by_Id(Resource):
+    def get(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return make_response(jsonify({"error": "Review not found"}), 404)
+
+        response_dict = review.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def patch(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return make_response(jsonify({"error": "Review not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(review, attr, value)
+
+        db.session.commit()
+        response_dict = review.to_dict()
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+    def delete(self, id):
+        review = Review.query.get(id)
+        if not review:
+            return make_response(jsonify({"error": "Review not found"}), 404)
+
+        db.session.delete(review)
+        db.session.commit()
+
+        response_dict = {"message": "Deleted successfully"}
+        response = make_response(jsonify(response_dict), 200)
+
+        return response
+
+api.add_resource(Review_by_Id, "/reviews/<int:id>")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
--- backend/models.py
+++ backend/models.py
@@ -1,38 +1,58 @@
-from app import db
 from sqlalchemy_serializer import SerializerMixin
+from app import db
+from sqlalchemy.orm import validates
+import re
 
-class Owner(db.Model, SerializerMixin):
-    __tablename__ = 'owners'
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     email = db.Column(db.String(120))
-    phone_number = db.Column(db.String(20))
+    phone_number = db.Column(db.String(50))
     password = db.Column(db.String(100))
-    
-    houses = db.relationship("House", backref="owner")
-    
-    def __repr__(self):
-        return f'<Owner: {self.first_name} {self.last_name}>'
+    user_type = db.Column(db.String(20))
 
-class Tenant(db.Model, SerializerMixin):
-    __tablename__ = 'tenants'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    email = db.Column(db.String(120))
-    phone_number = db.Column(db.String(20))
-    password = db.Column(db.String(100))
-    
-    houses = db.relationship("House", backref="tenant")
-    reviews = db.relationship("Review", backref="tenant")
-    
-    def __repr__(self):
-        return f'<Tenant: {self.first_name} {self.last_name}>'
+    properties_owned = db.relationship("Property", backref="owner", lazy="select")
+    payments_made = db.relationship("Payment", backref="tenant", lazy="select")
+    move_assistance_requests = db.relationship("MoveAssistance", backref="tenant", lazy="select")
+    reviews_written = db.relationship("Review", backref="tenant", lazy="select")
 
-class House(db.Model, SerializerMixin):
-    __tablename__ = 'houses'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'phone_number': self.phone_number,
+            'password': self.password,
+            'user_type': self.user_type
+        }
+
+    def __repr__(self):
+        return f'<User: {self.first_name} {self.last_name}>'
+    
+    @validates('password')
+    def validate_password(self, key, password):
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+
+        if not re.search(r'[A-Z]', password):
+            raise ValueError("Password must contain at least one uppercase letter.")
+
+        if not re.search(r'\d', password):
+            raise ValueError("Password must contain at least one digit.")
+
+        if not re.search(r'[!@#$%^&*()-=_+[\]{}|;:,.<>?]', password):
+            raise ValueError("Password must contain at least one special character.")
+
+        return password
+    
+class Property(db.Model, SerializerMixin):
+    __tablename__ = 'properties'
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     number_of_rooms = db.Column(db.Integer)
     categories = db.Column(db.String)
     location = db.Column(db.String)
@@ -40,39 +60,85 @@ class House(db.Model, SerializerMixin):
     description = db.Column(db.String)
     rating = db.Column(db.Float)
     image_urls = db.Column(db.ARRAY(db.String))
-    
-    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
-    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
-    reviews = db.relationship("Review", backref="house")
-    
+
+    payments_received = db.relationship("Payment", backref="property", lazy="select")
+    reviews_received = db.relationship("Review", backref="property", lazy="select")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'number_of_rooms': self.number_of_rooms,
+            'categories': self.categories,
+            'location': self.location,
+            'price': self.price,
+            'description': self.description,
+            'rating': self.rating,
+            'image_urls': self.image_urls,
+        }
+
     def __repr__(self):
-        return f'<House: {self.number_of_rooms} {self.description}>'
+        return f'<Property: {self.number_of_rooms} {self.description}>'
+
+class Payment(db.Model, SerializerMixin):
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float)
+    payment_date = db.Column(db.Date)
+    status = db.Column(db.String(20)) 
+    
+    tenant_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'payment_date': str(self.payment_date),  
+            'status': self.status,
+            'tenant_id': self.tenant_id,
+            'property_id': self.property_id,
+        }
+
+    def __repr__(self):
+        return f'<Payment: {self.id}>'
+
+class MoveAssistance(db.Model, SerializerMixin):
+    __tablename__ = 'move_assistance'
+    id = db.Column(db.Integer, primary_key=True)
+    service_details = db.Column(db.String)
+    status = db.Column(db.String(20))
+    
+    tenant_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'service_details': self.service_details,
+            'status': self.status,
+            'tenant_id': self.tenant_id,
+        }
+
+    def __repr__(self):
+        return f'<MoveAssistance: {self.id}>'
 
 class Review(db.Model, SerializerMixin): 
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
-    reviews = db.Column(db.String)
+    review_text = db.Column(db.String)
+    rating = db.Column(db.Integer)
     
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
-    tenant_id = db.Column(db.Integer, db.ForeignKey('tenants.id'))
-    
-    def __repr__(self):
-        return f'<Review: {self.reviews}>'
+    tenant_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
 
-class Owner_Tenant(db.Model, SerializerMixin):
-    __tablename__ = 'owners_tenants'
-    id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("owners.id"))
-    tenant_id = db.Column(db.Integer, db.ForeignKey("tenants.id"))
-    
-    def __repr__(self):
-        return f'<Owner_Tenant: {self.id} tenant: {self.tenant_id} owner: {self.owner_id}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'review_text': self.review_text,
+            'rating': self.rating,
+            'tenant_id': self.tenant_id,
+            'property_id': self.property_id,
+        }
 
-class Tenant_House(db.Model, SerializerMixin):
-    __tablename__ = 'tenants_houses'
-    id = db.Column(db.Integer, primary_key=True)
-    tenant_id = db.Column(db.Integer, db.ForeignKey("tenants.id"))
-    house_id = db.Column(db.Integer, db.ForeignKey("houses.id"))
-    
     def __repr__(self):
-        return f'<Tenant_House: {self.id} tenant_id={self.tenant_id} house: {self.house_id}>'
+        return f'<Review: {self.id}>'
