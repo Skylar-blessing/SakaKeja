9ec70d3e80af5ba267eaf584933d7106f2bab278
LimisiMike <michaellimisi7@gmail.com>
2023-08-02 14:30:09 +0300

    components edited

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Your navigation bar */}
+      {/* Your logo */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Look, Like, Love, Live.</h1>
+      <p>Discover our Amazing Homes with Our Real Estate Agency</p>
+    </div>
+  );
+};
+
+export default Welcome;
