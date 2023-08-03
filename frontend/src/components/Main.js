7a242e5dff29a269f676714c0b09548149b13360
LimisiMike <michaellimisi7@gmail.com>
2023-08-03 10:07:45 +0300

    modified files

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Welcome from './Welcome';
+// import Search from './Search';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <Welcome />
+      {/* Other sections/components */}
+    </div>
+  );
+};
+
+export default App;
