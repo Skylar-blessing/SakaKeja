81709bb066010dbe80bb63676afbdec2a5c8e85d
stephen <skaranja654@gmail.com>
2023-08-08 20:52:00 +0300

    pagination

--- frontend/src/compononents/PropertyList.js
+++ frontend/src/compononents/PropertyList.js
@@ -3,19 +3,26 @@ import PropertyCard from './PropertyCard';
 
 function PropertyList() {
   const [properties, setProperties] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const propertiesPerPage = 10;
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/properties')
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchProperties = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/properties?page=${currentPage}`);
+        const data = await response.json();
         if (data && Array.isArray(data.data)) {
           setProperties(data.data);
         } else {
           console.error('Invalid data format received from API:', data);
         }
-      })
-      .catch((error) => console.error('Error fetching properties:', error));
-  }, []);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+  
+    fetchProperties();
+  }, [currentPage]);
 
   const propertyCardsContainerStyle = {
     display: 'grid',
@@ -24,11 +31,24 @@ function PropertyList() {
     justifyItems: 'center',
   };
 
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div style={propertyCardsContainerStyle}>
-      {properties.map((property) => (
-        <PropertyCard key={property.id} property={property} />
-      ))}
+    <div>
+      <div style={propertyCardsContainerStyle}>
+        {properties.map((property) => (
+          <PropertyCard key={property.id} property={property} />
+        ))}
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        {Array.from({ length: Math.ceil(propertiesPerPage) }).map((_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
