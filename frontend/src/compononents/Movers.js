dad71799f5bd8287c35d73c0b51f254e88947062
stephen <skaranja654@gmail.com>
2023-08-08 16:28:48 +0300

    tenant dashboard

---
+++ frontend/src/compononents/Movers.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Movers() {
+return(
+    <div>
+        <h1>Movers</h1>
+    </div>
+)
+}
+export default Movers;
---
+++ frontend/src/compononents/Payments.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Payments() {
+return(
+    <div>
+        <h1>Payments</h1>
+    </div>
+)
+}
+export default Payments;
