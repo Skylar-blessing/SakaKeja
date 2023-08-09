234f1d3e4a516413721606764bbd5008158924ae
stephen <skaranja654@gmail.com>
2023-08-09 13:46:29 +0300

    Owner's menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import Login from './compononents/Login';
 import SignUp from './compononents/SignUp';
 import OwnerDashboard from './compononents/OwnerDashboard';
 import TenantDashboard from './compononents/TenantDashboard';
-import PropertyList from './compononents/PropertyList'
+import PropertyList from './compononents/PropertyList';
+import PostProperty from './compononents/PostProperty';
+import ViewProperties from './compononents/ViewProperties';
+import ViewPaymentBalance from './compononents/ViewPaymentBalance';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/owner-dashboard" element={<OwnerDashboard />} />
         <Route path="/tenant-dashboard" element={<TenantDashboard />} />
         <Route path="/rent" element={<PropertyList />} />
+        <Route path="/post-property" element={<PostProperty />} />
+        <Route path="/view-properties" element={<ViewProperties />} />
+        <Route path="/view-payment-balance" element={<ViewPaymentBalance />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/compononents/ViewProperties.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ViewProperties() {
+    return(
+        <div>
+            <h1>View Properties</h1>
+        </div>
+    )
+}
+
+export default ViewProperties;
